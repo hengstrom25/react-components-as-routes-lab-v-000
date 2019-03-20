07518cb286a4e0ad5199ed56a6d387d60c677ea0
Heidi Engstrom <hengstrom25@gmail.com>
2019-03-20 01:41:39 +0000

    Automatically backed up by Learn

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,6 +13,7 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
+<<<<<<< HEAD
       <div>
         <NavBar />
         <Route path="/" component={Home} />
@@ -20,6 +21,13 @@ const App = (props) => {
         <Route exact path="/movies" component={Movies} />
         <Route exact path="/actors" component={Actors} />
       </div>
+=======
+      <Route path="/" component={Home} />
+      <Route exact path="/actors" component={Actors} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/movies" component={Movies} />
+    </React.Fragment>
+>>>>>>> 897776fd351588e37234f8c516abc52eceaee3e0
     </Router>
   );
 };
